503aa1cff189185d2a55a78301c69d23eb957a87
keerthi <keerthipalimkar244@gmail.com>
2024-11-17 23:34:17 +0530

    First commit to maven java Project

---
+++ src/main/java/SEBatch2/MavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package SEBatch2.MavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
